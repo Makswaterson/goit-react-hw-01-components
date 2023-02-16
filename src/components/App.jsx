6f75b11619ad574e4da99ab2dd841e0b63bdcbff
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-02-16 20:18:38 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import data from '../data/data.json';
+import user from '../data/user.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import { Profile } from '../components/Profile/Profile';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +13,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
